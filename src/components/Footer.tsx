070e59001b530e89bf4ec62719b34db6c28d815a
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2025-04-01 00:35:19 +0530

    Refactor TrustedBySection padding; update HeroSection to use new background class; enhance meta tags in index.html for improved SEO

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Facebook, Instagram, XIcon, Youtube } from 'lucide-react'
+import { Facebook, Github, Instagram, XIcon, Youtube } from 'lucide-react'
 import { Link } from 'react-router-dom';
-
+import { ArrowRightCircle } from 'lucide-react';
 const Footer = () => {
   const scrollToTop = () => {
     window.scrollTo({
@@ -11,7 +11,21 @@ const Footer = () => {
   }
   return (
     <>
-      <div className=' bg-[url(/mario_running.webp)]  bg-repeat-x  bg-contain bg-bottom h-48 w-full'>
+      <div data-aos="fade-up" className='m-auto flex items-center justify-center flex-col gap-3 mb-10 mt-10 max-w-3xl'>
+        <h4 className='text-center text-3xl font-semibold text-white/80'>
+          Say Hello to <span className="text-gradient">VultiSig</span>! The ultimate self-custodial, multi-chain crypto vault.
+        </h4>
+
+        <div data-aos="fade-up" data-aos-duration="700" className="group flex lg:flex-row md:flex-row flex-col lg:gap-5 md:gap-5 gap-5 mt-10 items-center">
+          <button className="bg-gradient-to-r hover:scale-105 transition-all from-[#33E6BF] to-[#0439C7] text-design-dark lg:text-lg md:text-lg text-base font-medium lg:w-[14rem] md:w-[14rem] w-[19rem] py-3 rounded-2xl text-center flex items-center gap-3 justify-center">
+            <span>Join Discord</span>
+          </button>
+
+          <button className="bg-gradient-to-r hover:scale-105 transition-all border border-design-accent text-design-accent lg:text-lg md:text-lg text-base font-medium lg:w-[14rem] md:w-[14rem] w-[19rem] py-[0.72rem] rounded-2xl text-center flex items-center gap-3 justify-center">
+            <span>Download App</span>
+            <ArrowRightCircle className="text-[0px] transition-all ease-in-out group-has-[button]:text-xl" />
+          </button>
+        </div>
       </div>
       <footer className='dm-sans-regular pt-20 bg-gradient-to-t from-[#0b0d1b] to-design-dark w-full'>
         <div className='flex lg:flex-row md:flex-row flex-col items-start lg:px-10 md:px-5 px-10 lg:gap-6 md:gap-2 gap-16'>
@@ -20,13 +34,13 @@ const Footer = () => {
               Vultisig, the ultimate self-custodial, multi-chain <span className='text-gradient'> crypto vault.</span>
             </p>
             <div className='flex gap-4 my-10'>
-              <Link aria-label="Facebook Page" to="/" className='h-10 w-10 rounded-full flex justify-center items-center bg-white hover:bg-opacity-25 cursor-pointer bg-opacity-15 transition'>
-                <Facebook size={26} />
+              <Link aria-label="Facebook Page" to="https://github.com/Vultisig/" className='h-10 w-10 rounded-full flex justify-center items-center bg-white hover:bg-opacity-25 cursor-pointer bg-opacity-15 transition'>
+                <Github size={26} />
               </Link>
-              <Link aria-label="Instagram" to="https://instagram.com/retinaaiims" target='_blank' className='h-10 w-10 rounded-full flex justify-center items-center bg-white hover:bg-opacity-25 cursor-pointer bg-opacity-15 transition'>
+              <Link aria-label="Instagram" to="" target='_blank' className='h-10 w-10 rounded-full flex justify-center items-center bg-white hover:bg-opacity-25 cursor-pointer bg-opacity-15 transition'>
                 <Instagram size={26} />
               </Link>
-              <Link aria-label="Twitter" to="/" className='h-10 w-10 rounded-full flex justify-center items-center bg-white hover:bg-opacity-25 cursor-pointer bg-opacity-15 transition'>
+              <Link aria-label="Twitter" to="https://twitter.com/vultisig" className='h-10 w-10 rounded-full flex justify-center items-center bg-white hover:bg-opacity-25 cursor-pointer bg-opacity-15 transition'>
                 <XIcon size={26} />
               </Link>
               <a target='_blank' aria-label="Youtube" className='h-10 w-10 rounded-full flex justify-center items-center bg-white hover:bg-opacity-25 cursor-pointer bg-opacity-15 transition' href="https://youtube.com/@retinaaiims?si=YTNHaQk0fTtutvyR">
@@ -51,11 +65,11 @@ const Footer = () => {
                   Docs
                 </Link>
 
-                <a href="/#gallery" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <a href="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   $VULT
                 </a>
 
-                <Link onClick={scrollToTop} to="/team" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Extension
                 </Link>
               </div>
@@ -64,19 +78,19 @@ const Footer = () => {
             <div className='lg:max-w-md md:max-w-md flex-1 w-full'>
               <h5 className='text-base font-semibold flex items-center gap-2'><div className='size-2 rounded-full bg-design-accent'></div> Contact</h5>
               <div className='w-full flex flex-col gap-3 font-medium mt-5'>
-                <Link to="/accomodation" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Support
                 </Link>
 
-                <Link onClick={scrollToTop} to="/delegate-registration" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Audits
                 </Link>
 
-                <Link onClick={scrollToTop} to="/legal/privacy-policy" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/#faq" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   FAQs
                 </Link>
 
-                <Link onClick={scrollToTop} to="/legal/terms-of-service" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Contact Us
                 </Link>
               </div>
@@ -85,19 +99,19 @@ const Footer = () => {
             <div className='lg:max-w-md md:max-w-md w-full flex-1'>
               <h5 className='text-base font-semibold flex items-center gap-2'><div className='size-2 rounded-full bg-design-accent'></div> Legals</h5>
               <div className='w-full flex flex-col gap-3 font-medium mt-5'>
-                <Link onClick={scrollToTop} to="/legal/privacy-policy" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Privacy Policy
                 </Link>
 
-                <Link onClick={scrollToTop} to="/legal/terms-of-service" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Terms of Service
                 </Link>
 
-                <Link onClick={scrollToTop} to="/legal/code-of-conduct" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Code of Conduct
                 </Link>
 
-                <Link onClick={scrollToTop} to="/legal/code-of-conduct" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
+                <Link onClick={scrollToTop} to="/" className='bg-gray-400 hover:bg-opacity-25 cursor-pointer w-fit rounded-full bg-opacity-15 py-2 px-6'>
                   Compliance
                 </Link>
               </div>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,8 +2,8 @@ import { ArrowRight, ArrowRightCircle } from 'lucide-react';
 
 const HeroSection = () => {
   return (
-    <section className="relative bg-cover bg-center bg-[url(https://waiting-wrist-6f6.notion.site/image/attachment%3Ae3ac7d1a-0ad7-47c6-9034-7390c23f26e3%3ABackground_1.png?table=block&id=19123043-5592-80db-a763-c349dab75c61&spaceId=59ea08d2-bc07-4c80-b51a-dbd605835b75&width=1420&userId=&cache=v2)] pt-32 pb-20 px-6 md:px-12 overflow-hidden">
-      <div className="container mx-auto">
+    <section className="relative bg-image pt-32 pb-20 px-6 md:px-12 overflow-hidden">
+      <div data-aos="zoom-in" className="container mx-auto">
         <div className="max-w-3xl mx-auto text-center relative z-10">
           <a href='https://airdrop.vultisig.com/leaderboard' target='_blank' className='border m-auto mb-3 border-design-border cursor-pointer hover:scale-[102%] transition w-fit px-1 py-1 rounded-full flex items-center gap-2'>
             <div className='bg-design-card text-sm text-design-accent rounded-full py-0.5 px-2'>
@@ -25,7 +25,7 @@ const HeroSection = () => {
 
           <div className="flex flex-col sm:flex-row justify-center gap-4 ">
             {/* <Button className="bg-design-accent text-black hover:bg-design-accent-hover px-8 py-6 text-lg">Go to Airdrop</Button> */}
-            <div data-aos="fade-up" data-aos-duration="700" className="group flex lg:flex-row md:flex-row flex-col lg:gap-5 md:gap-5 gap-5 mt-10 items-center">
+            <div data-aos="fade-up" data-aos-duration="700" className="group flex lg:flex-row md:flex-row flex-col lg:gap-5 md:gap-5 gap-5 mt-0 items-center">
               <button className="bg-gradient-to-r hover:scale-105 transition-all from-[#33E6BF] to-[#0439C7] text-design-dark lg:text-lg md:text-lg text-base font-medium lg:w-[14rem] md:w-[14rem] w-[19rem] py-3 rounded-2xl text-center flex items-center gap-3 justify-center">
                 <span>Download App</span>
               </button>
